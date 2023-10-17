50ecc6aeb52eccb5412b098dc4500fc144d7baca
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-10-17 15:42:50 +0200

    add:raccolta dati

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Raccolta dati
+const nome = prompt("Scrivi il tuo Nome:");
+console.log(nome);
+const cognome = prompt("Scrivi il tuo Cognome:");
+console.log(cognome);
+const colore = prompt("Scrivi il tuo colore preferito:");
+console.log(colore);
