6afa0d1257922e5a788a8ac786e0e22f6a5c74db
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-10-17 15:51:14 +0200

    add: Logica e Output

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const cognome = prompt("Scrivi il tuo Cognome:");
 console.log(cognome);
 const colore = prompt("Scrivi il tuo colore preferito:");
 console.log(colore);
+
+// Logica del Programma
+const password = nome + cognome + colore + 21;
+console.log(password);
+const message = `La tua Password (insicurissima) generata è: ${password}`;
+
+// Output
+document.getElementById("result").innerHTML = message;
+
